82fef28fa94a2212419dbe09630b5cb5487409d0
Nicholas Mosier <nh.mosier@gmail.com>
2019-02-12 09:29:49 -0500

    fixed error handling

--- src/gadgets/ropaddr.cpp
+++ src/gadgets/ropaddr.cpp
@@ -4,29 +4,28 @@
 
 #include <unordered_set>
 #include <fstream>
+#include <iostream>
 //#include "ropaddr.h"
 
-extern "C" struct rop_addrs: std::unordered_set<uint64_t> {
-  rop_addrs(const char *path);
-};
-
-/* initialize set from filepath */
-rop_addrs::rop_addrs(const char *path) {
-  std::ifstream addrs_f;
-  addrs_f.open(path);
-
-  uint64_t val;
-  while ((addrs_f >> val)) {
-    insert(val);
-  }
-}
+extern "C" struct rop_addrs: std::unordered_set<uint64_t> {};
 
 extern "C" struct rop_addrs *rop_addrs_new(const char *path) {
-  try {
-    return new rop_addrs(path);
-  } catch (const std::exception &err) {
+  std::ifstream addrs_f;
+  
+  addrs_f.open(path, std::ifstream::in);
+  if (!addrs_f) {
     return NULL;
   }
+
+  struct rop_addrs *addrs = new rop_addrs();
+  
+  uint64_t val;
+  while ((addrs_f >> val)) {
+    addrs->insert(val);
+  }
+  addrs_f.close();
+  
+  return addrs;
 }
 
 extern "C" void rop_addrs_delete(struct rop_addrs *addrs) {
@@ -34,5 +33,5 @@ extern "C" void rop_addrs_delete(struct rop_addrs *addrs) {
 }
 
 extern "C" int rop_addrs_has(uint64_t val, struct rop_addrs *addrs) {
-  return addrs->find(val) == addrs->end();
+  return addrs->find(val) != addrs->end();
 }
