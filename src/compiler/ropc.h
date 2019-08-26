8c3e55566decc0c7fddcf6ec7fa108e5c4f781af
Nicholas Mosier <nh.mosier@gmail.com>
2019-08-26 16:23:39 -0400

    added ropc.h

---
+++ src/compiler/ropc.h
@@ -0,0 +1,14 @@
+/* ropc.h
+ * Nicholas Mosier 2019
+ */
+
+#ifndef __ROPC_H
+#define __ROPC_H
+
+#define ROPC_ADDR_CHK_NONE  0
+#define ROPC_ADDR_CHK_ALIGN 1
+#define ROPC_ADDR_CHK_POS   2
+
+#define ROPC_DEFAULT_STAGES 1
+
+#endif
